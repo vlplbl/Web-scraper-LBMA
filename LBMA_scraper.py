43dc682ab43c0ddab050423a209b72a69a2cb8c5
vlplbl <vlplbl@abv.bg>
2019-10-01 10:39:26 +0300

    v 1.0

--- LBMA_scraper.py
+++ LBMA_scraper.py
@@ -1,75 +1,87 @@
-print('Starting...')
-
-import os
-from time import sleep
-from datetime import date
-import pandas as pd
-import requests
 from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import datetime
+from time import sleep
+import os
+import openpyxl
+import xlsxwriter
 
 
-date = date.today()
-year = date.year
-day = date.day
+class Scraper:
+    def __init__(self):
+        print('Starting...')
+        self.year = datetime.date.year
+
+    def get_price(self, metal, num_cols, table_key='tbody'):
+        if metal == 'copper':
+            url = 'https://www.westmetall.com/en/markdaten.php?action=show_table&field=LME_Cu_cash'
+        else:
+            url = f'http://lbma.datanauts.co.uk/table?metal={metal}&year={self.year}&type=daily'
+        print(f'Connecting to: {url}')
+        try:
+            source = requests.get(url).text
+            soup = BeautifulSoup(source, 'lxml')
+        except:
+            print("Can't find the page")
+
+        table = soup.find(table_key)
+        items = []
+        try:
+            print(f'Downloading {metal} prices... ')
+            for item in table.find_all('td'):
+                items.append(item.text)
+            rows = []
+            for i in range(0, len(items), num_cols):
+                rows.append(items[i:i+num_cols])
+            return rows
+        except:
+            print("Can't find the data on that page")
+
+    def create_dataframes(self):
+        gold = self.get_price('gold', 7)
+        gold_frame = pd.DataFrame(
+            gold, columns=['Date', 'AM', 'PM', 'AM(GBP)', 'PM(GBP)', 'AM(EUR)', 'PM(EUR)'])
+        gold_frame = gold_frame.iloc[:, :3]
+        gold_frame['AM'] = gold_frame['AM'].astype('float64').round(decimals=4)
+        gold_frame['PM'] = gold_frame['PM'].astype('float64').round(decimals=4)
+
+        sleep(1)
+        silver = self.get_price('silver', 4)
+        silver_frame = pd.DataFrame(
+            silver, columns=['Date', 'Ag', 'GBP', 'EUR'])
+        silver_frame = silver_frame.iloc[:, :2]
+        silver_frame['Ag'] = silver_frame['Ag'].astype(
+            'float64').round(decimals=4)
+
+        sleep(1)
+        copper = self.get_price('copper', 4, table_key='table')
+        copper_frame = pd.DataFrame(
+            copper, columns=['Date', 'Cu', 'Cu_3M', 'Cu_stock'])
+        copper_frame = copper_frame.iloc[:, :2]
+        copper_frame.Cu = copper_frame.Cu.str.split('.')
+        copper_frame.Cu = copper_frame.Cu.str.join("")
+        copper_frame.Cu = copper_frame.Cu.str.replace(',', '.')
+        copper_frame.Cu = copper_frame.Cu.astype('float64')
+        copper_frame.Date = copper_frame.Date.str.split('.')
+        copper_frame.Date = copper_frame.Date.str.join("")
+        copper_frame.Date = copper_frame.Date.str.replace(" ", "-")
+        copper_frame.Date = pd.to_datetime(copper_frame.Date, errors='coerce')
+        copper_frame.Date = copper_frame.Date.dt.date
+
+        df = pd.merge(gold_frame, silver_frame, how='outer')
+        df.Date = pd.to_datetime(df.Date)
+        df.Date = df.Date.dt.date
+        df = pd.merge(df, copper_frame, how='outer')
+
+        with pd.ExcelWriter('Prices.xlsx', engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, freeze_panes=(1, 1))
+            worksheet = writer.sheets['Sheet1']
+            width = df.Date.astype(str).map(len).max()
+            worksheet.set_column('A:A', width)
 
 
-def get_gold_price():
-    link = f'http://lbma.datanauts.co.uk/table?metal=gold&year={year}&type=daily'
-    print(f'Connecting to: {link}')
-    try:
-        source = requests.get(link).text
-        soup = BeautifulSoup(source, 'lxml')
-    except:
-        print("Can't find the page")
-
-    table = soup.find('tbody')
-    items = []
-    try:
-        print('Downloading gold prices... ')
-        for item in table.find_all('td'):
-            items.append(item.text)
-        rows = []
-        for i in range(0, len(items), 7):
-            rows.append(items[i:i+7])
-        return rows
-    except Exception as e:
-        print("Can't find the data on that page")
-
-
-def get_silver_price():
-    link = f'http://lbma.datanauts.co.uk/table?metal=silver&year={year}&type=daily'
-    print(f'Connecting to: {link}')
-    try:
-        source = requests.get(link).text
-        soup = BeautifulSoup(source, features="lxml")
-    except:
-        print("Can't find the page")
-
-    table = soup.find('tbody')
-    items = []
-    try:
-        print('Downloading silver prices...')
-        for item in table.find_all('td'):
-            items.append(item.text)
-        rows = []
-        for i in range(0, len(items), 4):
-            rows.append(items[i:i+4])
-        return rows
-    except Exception as e:
-        print("Can't find the data on that page")
-
-
-gold = get_gold_price()
-gold_frame = pd.DataFrame(
-    gold, columns=['Date', 'AM', 'PM', 'AM(GBP)', 'PM(GBP)', 'AM(EUR)', 'PM(EUR)'])
-gold_frame = gold_frame.iloc[:, :3]
-sleep(1)
-silver = get_silver_price()
-silver_frame = pd.DataFrame(
-    silver, columns=['Date', 'Ag', 'GBP', 'EUR'])
-silver_frame = silver_frame.iloc[:, :2]
-df = pd.merge(gold_frame, silver_frame, on=['Date', 'Date'])
-
-df.to_excel('Prices.xlsx', index=False)
+scraper = Scraper()
+scraper.create_dataframes()
 print('Done')
 os.system('pause')
